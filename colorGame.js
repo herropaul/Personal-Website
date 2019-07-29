77a2bb210f1305e2f7f2cbbc51735b671b2b7d0c
Paul Nguon <35973829+herropaul@users.noreply.github.com>
2019-07-28 23:29:53 -0700

    Update colorGame.js

--- colorGame.js
+++ colorGame.js
@@ -1,4 +1,71 @@
 var colors = new Array(6);
+var box = document.getElementsByClassName("box");
+var header = document.querySelector("header");
+var goalColor = colors[randomIndex];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = goalColor;
+var messageDisplay = document.querySelector("#message");
+var resetButton = document.querySelector("button");
+var hardBtn = document.getElementById("hardButton");
+var easyBtn = document.getElementById("easyButton");
+
+easyBtn.addEventListener("click", function(){
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    colors = new Array(3);
+    for(var i = 0; i < colors.length; i++){
+        var randomIndex = Math.floor(Math.random()*colors.length);
+        var r = Math.floor(Math.random() * Math.floor(255));
+        var g = Math.floor(Math.random() * Math.floor(255));
+        var b = Math.floor(Math.random() * Math.floor(255));
+        var colVal = "rgb(" + r + ", " + g + ", " + b + ")";
+        colors[i] = colVal;
+    }
+    goalColor = colors[randomIndex];
+    colorDisplay.textContent = goalColor;
+
+    for(var i = 0; i < box.length; i++){
+        if(colors[i]){
+            box[i].style.backgroundColor = colors[i];
+            //console.log(box[i].style.backgroundColor);
+        }
+        else{
+            box[i].style.display = "none";
+        }
+    }
+
+    messageDisplay.textContent = "";
+    header.style.backgroundColor = "#659DBD";
+    //console.log("*******");
+});
+
+hardBtn.addEventListener("click", function(){
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    colors = new Array(6);
+
+    for(var i = 0; i < colors.length; i++){
+        var randomIndex = Math.floor(Math.random()*colors.length);
+        var r = Math.floor(Math.random() * Math.floor(255));
+        var g = Math.floor(Math.random() * Math.floor(255));
+        var b = Math.floor(Math.random() * Math.floor(255));
+        var colVal = "rgb(" + r + ", " + g + ", " + b + ")";
+        colors[i] = colVal;
+    }
+    goalColor = colors[randomIndex];
+    colorDisplay.textContent = goalColor;
+
+    for(var i = 0; i < box.length; i++){
+        box[i].style.backgroundColor = colors[i];
+        box[i].style.display = "block";
+        //console.log(box[i].style.backgroundColor);
+    }
+
+    messageDisplay.textContent = "";
+    header.style.backgroundColor = "#659DBD";
+    //console.log("************");
+
+});
 
 
 // sets random rgb values to colors array
@@ -11,13 +78,8 @@ for(var i = 0; i < colors.length; i++){
     colors[i] = colVal;
 }
 
-var box = document.getElementsByClassName("box");
-var header = document.querySelector("header");
-var goalColor = colors[randomIndex];
-var colorDisplay = document.getElementById("colorDisplay");
+goalColor = colors[randomIndex];
 colorDisplay.textContent = goalColor;
-var messageDisplay = document.querySelector("#message");
-var resetButton = document.querySelector("button");
 
 function reset(){
     for(var i = 0; i < colors.length; i++){
